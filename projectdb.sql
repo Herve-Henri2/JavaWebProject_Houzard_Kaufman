b6f07a9deb3267382ec94cf6881504de29adc15b
QuentinK <quentinkaufman2000@gmail.com>
2021-12-12 17:03:32 +0100

    project database created

---
+++ projectdb.sql
@@ -0,0 +1,28 @@
+drop Database if exists projectdb;
+create DATABASE if not exists projectdb;
+use projectdb;
+
+drop table if exists user;
+create table user
+(
+username varchar(45) primary key,
+password varchar(45),
+role varchar(15)
+);
+
+drop table if exists todo;
+create table todo
+(
+id integer primary key,
+role varchar(600)
+);
+
+insert into user value('Antoine', '123','student');
+insert into user value('Marc', '321','teacher');
+select * from user;
+
+insert into todo value(1, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+insert into todo value(2, 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
+select * from todo;
+
+
