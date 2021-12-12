9dbafd1f2e65da77c45d58cf703a4b106972d8f8
Herve-Henri <rvhouzard@gmail.com>
2021-12-12 17:32:03 +0100

    .

---
+++ src/main/java/MainPackage/MainServlet.java
@@ -0,0 +1,31 @@
+package MainPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		//out.println("This is the Login Screen");
+		request.getRequestDispatcher("WEB-INF/login.jsp").forward(request,
+				response);
+	}
+
+}
